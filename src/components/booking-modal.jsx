e4dfb58b03dabf722b1a96a4a0d15477ada22f69
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-10-24 18:31:11 +0530

    Added Booking Modal

---
+++ src/components/booking-modal.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../css/booking-modal.css";
+
+const BookingModal = ({ hotelName, onClose }) => {
+  return (
+    <div className="booking-modal-section">
+      <div className="booking-modal swipe">
+        <div className="hide-modal hide-modal-bg" onClick={onClose}>
+          <i className="fa-solid fa-circle-xmark"></i>
+        </div>
+        <div className="booking-modal-banner">
+          <img
+            src="https://files.jotform.com/jufs/ugurg/form_files/hotel-banner.63885da4032201.67905415.png?md5=CPU4YwPmo5i09ZNIUzZzTA&expires=1729767274"
+            alt=""
+          />
+        </div>
+        <h2 className="booking-hotel-name">{hotelName}</h2>{" "}
+        {/* Updated to use hotelName */}
+        <p className="booking-hotel-description">
+          Book your stay at {hotelName} for an unforgettable escape!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BookingModal;
--- src/components/hotel-card.jsx
+++ src/components/hotel-card.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/hotel-card.css";
+import BookingModal from "./booking-modal";
 
 const HotelCard = ({
   image,
@@ -10,43 +11,57 @@ const HotelCard = ({
   rooms,
   website,
 }) => {
+  const [showBookingModal, setShowBookingModal] = useState(false);
+
+  const handleBookinghModal = () => {
+    setShowBookingModal(true);
+  };
+
+  const handleCloseBookingModal = () => {
+    setShowBookingModal(false);
+  };
   return (
-    <div className="hotel-card swipe">
-      <div className="hotel-card-image">
-        <img src={image} alt={name} loading="lazy" />
-      </div>
-      <div className="hotel-card-content">
-        <div className="hotel-card-content-header">
-          <h1>{name}</h1>
-          <h4>
-            <span>₹ {price}</span> /Night
-            {/* <span>- Valid Only for Season</span> */}
-          </h4>
-          <p>
+    <>
+      {showBookingModal && (
+        <BookingModal hotelName={name} onClose={handleCloseBookingModal} />
+      )}
+      <div className="hotel-card swipe">
+        <div className="hotel-card-image">
+          <img src={image} alt={name} loading="lazy" />
+        </div>
+        <div className="hotel-card-content">
+          <div className="hotel-card-content-header">
+            <h1>{name}</h1>
+            <h4>
+              <span>₹ {price}</span> /Night
+              {/* <span>- Valid Only for Season</span> */}
+            </h4>
             <p>
-              <span>
-                {Array.from({ length: rating }, (_, index) => (
-                  <i key={index} className="fa-solid fa-star"></i>
-                ))}
-              </span>
-              {` ${rating} Stars`}
+              <p>
+                <span>
+                  {Array.from({ length: rating }, (_, index) => (
+                    <i key={index} className="fa-solid fa-star"></i>
+                  ))}
+                </span>
+                {` ${rating} Stars`}
+              </p>
+              <p>
+                <a href={website} target="_blank">
+                  Website <i class="fa-solid fa-up-right-from-square"></i>
+                </a>
+              </p>
             </p>
-            <p>
-              <a href={website} target="_blank">
-                Website <i class="fa-solid fa-up-right-from-square"></i>
-              </a>
-            </p>
-          </p>
-        </div>
-        <div className="hotel-card-content-center">
-          <p>{description}</p>
-        </div>
-        <div className="hotel-card-content-footer">
-          <h4>{rooms} Rooms</h4>
-          <button>Book Now</button>
+          </div>
+          <div className="hotel-card-content-center">
+            <p>{description}</p>
+          </div>
+          <div className="hotel-card-content-footer">
+            <h4>{rooms} Rooms</h4>
+            <button onClick={handleBookinghModal}>Book Now</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
