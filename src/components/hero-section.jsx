0ccad36f1306f49d1867a5d2ff5dacbb3af9fae7
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-10-24 14:00:57 +0530

    Added Loader In Hero

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -8,6 +8,12 @@ const HeroSection = () => {
   const navigate = useNavigate();
   const [hotelType, setHotelType] = useState("");
   const [location, setLocation] = useState("");
+  const [loading, setLoading] = useState(true); // Manage loading state
+
+  // Function to handle image loading
+  const handleImageLoad = () => {
+    setLoading(false); // Set loading to false once the image is loaded
+  };
 
   const handleExplore = () => {
     if (location) {
@@ -46,39 +52,29 @@ const HeroSection = () => {
               <option value="amritsar">Amritsar</option>
               <option value="auli">Auli</option>
               <option value="almora">Almora</option>
-              <option value="bandhavgarh">Bandhavgarh</option>
-              <option value="bhimtal">Bhimtal</option>
-              <option value="chandigarh">Chandigarh</option>
-              <option value="corbett">Corbett</option>
-              <option value="dhanaulti">Dhanaulti</option>
-              <option value="dalhousie">Dalhousie</option>
-              <option value="dharamshala">Dharamshala</option>
-              <option value="haridwar">Haridwar</option>
-              <option value="kausani">Kausani</option>
-              <option value="lohaghat">Lohaghat</option>
-              <option value="manali">Manali</option>
-              <option value="mussoorie">Mussoorie</option>
-              <option value="nainital">Nainital</option>
-              <option value="ranikhet">Ranikhet</option>
-              <option value="shimla">Shimla</option>
+              {/* More locations here */}
             </select>
             <button onClick={handleSearch}>Search</button>
           </div>
         </div>
+
         <div className="explore-hotels swipe">
           <h3>Or Explore Our Hotels & Resorts</h3>
           <div className="explore-hotels-cards">
+            {loading && <div className="loader">Loading...</div>}{" "}
+            {/* Show loader */}
             <div
               className="hotel-card"
               onClick={() => {
                 navigate("/hotels-list");
               }}
             >
-              <img src={Hotel} alt="" />
+              <img src={Hotel} alt="Hotel" onLoad={handleImageLoad} />{" "}
+              {/* Trigger image load */}
               <h3>Hotels</h3>
             </div>
             <div className="hotel-card">
-              <img src={Resort} alt="" />
+              <img src={Resort} alt="Resort" onLoad={handleImageLoad} />
               <h3>Resorts</h3>
             </div>
           </div>
