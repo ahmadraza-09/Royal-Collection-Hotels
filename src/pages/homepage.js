9724cc8f58e8201b6b30120aeb0fa8729cddb7d0
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-10-30 15:09:55 +0530

    Changes

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,33 +1,49 @@
-import React, { Suspense, lazy } from "react";
-import Navbar from '../components/navbar'
-import TopBanner from '../components/top-banner'
-// import HeroSection from '../components/hero-section'
-import Footer from '../components/footer'
-import WhatWeOffer from '../components/what-we-offer'
-import WhyChooseUs from '../components/why-choose-us'
-import DestinationSliderSection from '../components/destination-slider-section'
-import CustomerCounting from '../components/customer-counting'
-import PopularHotels from '../components/popular-hotels'
+import React, { useEffect, useState, Suspense, lazy } from "react";
+import Navbar from '../components/navbar';
+import TopBanner from '../components/top-banner';
+import Footer from '../components/footer';
+import WhatWeOffer from '../components/what-we-offer';
+import WhyChooseUs from '../components/why-choose-us';
+import DestinationSliderSection from '../components/destination-slider-section';
+import CustomerCounting from '../components/customer-counting';
+import PopularHotels from '../components/popular-hotels';
 import Loader from "../assets/loader/loader2.gif";
+
 const HeroSection = lazy(() => import("../components/hero-section"));
 
 const HomePage = () => {
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, []);
 
     return (
         <>
-            {<TopBanner />}
-            {<Navbar />}
-            <Suspense fallback={<div className="loader"><img src={Loader} alt="" /></div>}>
-                {<HeroSection />}
-            </Suspense>
-            {<WhyChooseUs />}
-            {<WhatWeOffer />}
-            {<DestinationSliderSection />}
-            {<CustomerCounting />}
-            {<PopularHotels />}
-            {<Footer />}
+            {loading ? (
+                <div className="loader">
+                    <img src={Loader} alt="Loading..." />
+                </div>
+            ) : (
+                <>
+                    <TopBanner />
+                    <Navbar />
+                    <HeroSection />
+                    <WhyChooseUs />
+                    <WhatWeOffer />
+                    <DestinationSliderSection />
+                    <CustomerCounting />
+                    <PopularHotels />
+                    <Footer />
+                </>
+            )}
         </>
-    )
-}
+    );
+};
 
-export default HomePage
+export default HomePage;
