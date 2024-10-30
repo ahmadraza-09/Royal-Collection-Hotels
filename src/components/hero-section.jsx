299be65a052c5d4b39be245307df23db1caf0458
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-10-30 12:47:13 +0530

    Changes

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -11,10 +11,6 @@ const HeroSection = () => {
   const [location, setLocation] = useState("");
   const [loading, setLoading] = useState(true); // Manage loading state
 
-  useEffect(() => {
-    setLoading(false);
-  }, []);
-
   // Function to handle image loading
   const handleImageLoad = () => {
     setLoading(false); // Set loading to false once the image is loaded
@@ -34,90 +30,84 @@ const HeroSection = () => {
 
   return (
     <>
-      {loading ? (
-        <div className="loader">
-          <img src={Loader} alt="" />
-        </div>
-      ) : (
-        <div className="hero-section">
-          <div className="overlay">
-            <h2 className="swipe">Explore Amazing India</h2>
-            <div className="find-hotels swipe">
-              <h4>Find Great Places To Stay</h4>
-              <div className="hotel-search-comp">
-                <select
-                  name="hotelType"
-                  value={hotelType}
-                  onChange={(e) => setHotelType(e.target.value)}
-                >
-                  <option value="">Choose Hotels & Resorts</option>
-                  <option value="hotels">Hotels</option>
-                  <option value="resorts">Resorts</option>
-                </select>
-                <select
-                  name="location"
-                  value={location}
-                  onChange={(e) => setLocation(e.target.value)}
-                >
-                  <option value="">Choose Location</option>
-                  <option value="amritsar">Amritsar</option>
-                  <option value="auli">Auli</option>
-                  <option value="almora">Almora</option>
-                  <option value="bandhavgarh">Bandhavgarh</option>
-                  <option value="bhimtal">Bhimtal</option>
-                  <option value="chandigarh">Chandigarh</option>
-                  <option value="corbett">Corbett</option>
-                  <option value="dhanaulti">Dhanaulti</option>
-                  <option value="dalhousie">Dalhousie</option>
-                  <option value="dharamshala">Dharamshala</option>
-                  <option value="haridwar">Haridwar</option>
-                  <option value="kausani">Kausani</option>
-                  <option value="lohaghat">Lohaghat</option>
-                  <option value="manali">Manali</option>
-                  <option value="mussoorie">Mussoorie</option>
-                  <option value="nainital">Nainital</option>
-                  <option value="ranikhet">Ranikhet</option>
-                  <option value="shimla">Shimla</option>
-                  <option value="mukteshwar">Mukteshwar</option>
-                  <option value="chail">Chail</option>
-                </select>
-                <button onClick={handleSearch}>Search</button>
-              </div>
+      <div className="hero-section">
+        <div className="overlay">
+          <h2 className="swipe">Explore Amazing India</h2>
+          <div className="find-hotels swipe">
+            <h4>Find Great Places To Stay</h4>
+            <div className="hotel-search-comp">
+              <select
+                name="hotelType"
+                value={hotelType}
+                onChange={(e) => setHotelType(e.target.value)}
+              >
+                <option value="">Choose Hotels & Resorts</option>
+                <option value="hotels">Hotels</option>
+                <option value="resorts">Resorts</option>
+              </select>
+              <select
+                name="location"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+              >
+                <option value="">Choose Location</option>
+                <option value="amritsar">Amritsar</option>
+                <option value="auli">Auli</option>
+                <option value="almora">Almora</option>
+                <option value="bandhavgarh">Bandhavgarh</option>
+                <option value="bhimtal">Bhimtal</option>
+                <option value="chandigarh">Chandigarh</option>
+                <option value="corbett">Corbett</option>
+                <option value="dhanaulti">Dhanaulti</option>
+                <option value="dalhousie">Dalhousie</option>
+                <option value="dharamshala">Dharamshala</option>
+                <option value="haridwar">Haridwar</option>
+                <option value="kausani">Kausani</option>
+                <option value="lohaghat">Lohaghat</option>
+                <option value="manali">Manali</option>
+                <option value="mussoorie">Mussoorie</option>
+                <option value="nainital">Nainital</option>
+                <option value="ranikhet">Ranikhet</option>
+                <option value="shimla">Shimla</option>
+                <option value="mukteshwar">Mukteshwar</option>
+                <option value="chail">Chail</option>
+              </select>
+              <button onClick={handleSearch}>Search</button>
             </div>
+          </div>
 
-            <div className="explore-hotels swipe">
-              <h3>Or Explore Our Hotels & Resorts</h3>
-              <div className="explore-hotels-cards">
-                {/* Show loader */}
-                <div
-                  className="hotel-card"
-                  onClick={() => {
-                    navigate("/hotels-list");
-                  }}
-                >
-                  <img
-                    src={Hotel}
-                    alt="Hotel"
-                    onLoad={handleImageLoad}
-                    loading="lazy"
-                  />{" "}
-                  {/* Trigger image load */}
-                  <h3>Hotels</h3>
-                </div>
-                <div className="hotel-card">
-                  <img
-                    src={Resort}
-                    alt="Resort"
-                    onLoad={handleImageLoad}
-                    loading="lazy"
-                  />
-                  <h3>Resorts</h3>
-                </div>
+          <div className="explore-hotels swipe">
+            <h3>Or Explore Our Hotels & Resorts</h3>
+            <div className="explore-hotels-cards">
+              {/* Show loader */}
+              <div
+                className="hotel-card"
+                onClick={() => {
+                  navigate("/hotels-list");
+                }}
+              >
+                <img
+                  src={Hotel}
+                  alt="Hotel"
+                  onLoad={handleImageLoad}
+                  loading="lazy"
+                />{" "}
+                {/* Trigger image load */}
+                <h3>Hotels</h3>
+              </div>
+              <div className="hotel-card">
+                <img
+                  src={Resort}
+                  alt="Resort"
+                  onLoad={handleImageLoad}
+                  loading="lazy"
+                />
+                <h3>Resorts</h3>
               </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
