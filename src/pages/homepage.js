131a35e7eb82c38cf532231af8a1c2c0444561b8
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-10-30 14:45:42 +0530

    added suspense

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Suspense, lazy } from "react";
 import Navbar from '../components/navbar'
 import TopBanner from '../components/top-banner'
-import HeroSection from '../components/hero-section'
+// import HeroSection from '../components/hero-section'
 import Footer from '../components/footer'
 import WhatWeOffer from '../components/what-we-offer'
 import WhyChooseUs from '../components/why-choose-us'
 import DestinationSliderSection from '../components/destination-slider-section'
 import CustomerCounting from '../components/customer-counting'
 import PopularHotels from '../components/popular-hotels'
+const HeroSection = lazy(() => import("../components/hero-section"));
 
 const HomePage = () => {
 
@@ -15,7 +16,9 @@ const HomePage = () => {
         <>
             {<TopBanner />}
             {<Navbar />}
-            {<HeroSection />}
+            <Suspense fallback={<div className="loader">Loading...</div>}>
+                {<HeroSection />}
+            </Suspense>
             {<WhyChooseUs />}
             {<WhatWeOffer />}
             {<DestinationSliderSection />}
